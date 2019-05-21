dce30d3eff8363851df0f34ba883b216a76da118
Jennie Robinson Faber <jennie@jenniefaber.com>
2019-05-21 12:40:35 -0400

    Update gridsome.client.js

--- gridsome.client.js
+++ gridsome.client.js
@@ -3,6 +3,10 @@ import VModal from 'vue-js-modal/dist/ssr.index'
 export default function (Vue, options, { isServer: disabled, router }) {
   if(process.isClient) {
     const { default: VModal } = require('vue-js-modal')
-    Vue.use(VModal)
+    Vue.use(VModal, {
+      router,
+      disabled,
+      ...options
+    })
   }
 }
